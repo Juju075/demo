39ffb5b9f31d04ddc8646455ed2f89885cd294f8
elgato <amzfba.1bestbuy@gmail.com>
2020-03-06 12:26:15 +0100

    update

--- log_out.php
+++ log_out.php
@@ -1,6 +1,5 @@
 <?php 
 session_start();
-
 // Suppression des variables de session et de la session
 $_SESSION = array();
 session_destroy();
@@ -9,6 +8,6 @@ session_destroy();
 setcookie('login', '');
 setcookie('pass_hache', '');
 
-header('location: https://www.google.fr/');
+header('location: index.php');
 
 ?>
