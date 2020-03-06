881592e30109253908a32b72687c5679930e20f2
elgato <amzfba.1bestbuy@gmail.com>
2020-03-06 11:06:33 +0100

    rename class title

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php
+         //remplissage automatique login et mot de passe  
+           
+           if(isset($_COOKIE['user_id']              )){
+
+                //verifie dans la bdd si 
+                echo 'Votre ID de session est le ' .$_COOKIE['user_id'];
+            }
+?>
 
 <!DOCTYPE html />
 <html>
@@ -10,36 +19,30 @@
 <link rel="icon" href="favicon.ico">
 
 <link href="css/index.css" rel="stylesheet" type="text/css">
-
-
-<link rel="stylesheet" type="text/css" href="slick/slick.css"/>
-<link rel="stylesheet" type="text/css" href="slick/slick-theme.css"/>
-
-
 </head>
 <body>
-<div class ="container">
+<div id ="container">
 
     <div class ="elements"><!-- elements 1-->
     <img src="images/cadre.jpg" alt="image" height="500px">
     </div><!-- Fin elements 1 -->
 
-    <div class="elements"><!-- elements 2-->
-        <div class="sous_elements"><img src="images/cadre2.jpg" alt="image" height="130px"></div><!-- Fin sous_elements-1 -->
+    <div class="bloc_elements"><!-- elements 2-->
+        <div class="sous_elements"><img src="images/gbaf_logo.jpg" alt="image" height="130px"></div><!-- Fin sous_elements-1 -->
         <div class="sous_elements"><p>BIENVENUE<br>
                 sur la page d'identification de votre Extranet<br> (espace réservé aux adhérents GBAF).<br>
-                En vous connectant sur ce site,vous acceptez l'utilisation de cookies à des fins de mesure d'audience.<br> <strong><a href="details.php">En savoir plus</a></strong></p>
+                En vous connectant sur ce site,vous acceptez l'utilisation de<br> cookies à des fins de mesure d'audience.<br> <strong><a href="https://www.cnil.fr/professionnel" target="_blank">En savoir plus</a></strong></p>
         </div><!-- Fin sous_elements-2 -->
 
         <div class="sous_elements">
             <form method="post" action="php/execution.php">
                     <input  type="text" name="utilisateur" placeholder="Login ou email" required>
                     <input type= "password" name= "mot_de_passe" placeholder="Votre mot de passe" minlenght= ="6"  required/><br>
-                    <input type="checkbox" name="connexion" id="connexion"><p>Rester connecté.</p>
-                    <!-- <?php if ($error = 'false') {
-                        echo '<p font style="color:red;">Mauvais identifiant ou mot de passe !</p>';
-                    }else{} 
-                    ?> -->
+                    <input type="checkbox" name="connexion" id="connexion" ><br>
+                    <!-- $smg si message 
+                        
+                    
+                    <p font style="color:red;">Mauvais identifiant ou mot de passe !</p> -->
                     <br><input class="envoyer" type= "submit" value=" Valider"/>
             </form>
         </div><!-- Fin sous_elements-3 -->
@@ -57,23 +60,9 @@
 
 </div><!-- Fin main wrap -->
 
-<div class="container_2">
+<div id="footer_container">
     <footer class="sous_elements">
-        <p1>Féderation des banques française - CONFIDENTIEL SAUF MENTION CONTRAIRE.Données personnelles - <a href"">Mentions légales</a>
-        <br>elgatodesign@2020</p1><br>
-        <img class="img1" src="images/banque_01.png" height="60px"><img class="img1"src="images/banque_02.png" height="50px"/><img class="img1" src="images/banque_03.png" height="60px"/><img class="img1" src="images/banque_04.png" height="60px"/><img src="images/banque_05.png" height="60px"/><img src="images/banque_07.png" height="60px"/>
-        <!-- Barre defilant avec logo -->
-        <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-            <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-            <script type="text/javascript" src="slick/slick.min.js"></script>
-                                        
-            <script type="text/javascript">
-            $(document).ready(function(){
-            $('.your-class').slick({
-            setting-name: setting-value
-            });
-            });
-            </script>    
+        <h5>Le Groupement Banque Assurance Français - CONFIDENTIEL SAUF MENTION CONTRAIRE. - <a href="mentions_legales.php">Mentions légales</a>
 </footer>   
 </div><!-- Fin container 2-->                       
 <body>
