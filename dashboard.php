d3004f4ff7ba2c01f418c3339373269f5038da5f
elgato <amzfba.1bestbuy@gmail.com>
2020-03-06 12:31:19 +0100

    update

--- dashboard.php
+++ dashboard.php
@@ -1,44 +1,59 @@
 
  <?php
 session_start();
+if(isset($_SESSION['utilisateur']) AND !empty ($_SESSION['utilisateur'])){
 
-$bdd = new PDO('mysql:host=localhost;dbname=mon_projet;charset=UTF8', 'dev06' ,'_cxeK9Dt)hkA', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-$req_prenom = $bdd->prepare('SELECT prenom FROM banksters WHERE utilisateur = ? ');
-$req_prenom->execute(array($_SESSION['utilisateur']));
-$prenom = $req_prenom->fetch();
 
-$req_nom_de_famille = $bdd->prepare('SELECT nom FROM banksters WHERE utilisateur = ? ');
-$req_nom_de_famille->execute(array($_SESSION['utilisateur']));
-$nomfamille = $req_nom_de_famille->fetch();
+    //interdir l(acces a la page si session null)
+    $bdd = new PDO('mysql:host=localhost;dbname=mon_projet;charset=UTF8', 'dev06' ,'_cxeK9Dt)hkA', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    $req_prenom = $bdd->prepare('SELECT prenom FROM banksters WHERE utilisateur = ? ');
+    $req_prenom->execute(array($_SESSION['utilisateur']));
+    $prenom = $req_prenom->fetch();
 
-/*
-//Debuging
-var_dump($_SESSION['prenom']);
-echo $prenom[0];
-echo $nomfamille[0];
-//------------------------------
-*/ 
+    $req_nom_de_famille = $bdd->prepare('SELECT nom FROM banksters WHERE utilisateur = ? ');
+    $req_nom_de_famille->execute(array($_SESSION['utilisateur']));
+    $nomfamille = $req_nom_de_famille->fetch();
 
-$likes = null;
+    /*
+    //Debuging
+    var_dump($_SESSION['prenom']);
+    echo $prenom[0];
+    echo $nomfamille[0];
+    //------------------------------
+    */ 
 
-//Template sans marqueurs
+    $likes = null;
 
-// Requete tab 
-$req_etablissements = $bdd->prepare('SELECT nom, descriptif FROM etablissements');
-$req_etablissements->execute();
-$tabfinals = $req_etablissements->fetchall();
+    $req_info = $bdd->prepare('SELECT avatar FROM banksters WHERE id_user = ?');
+    $req_info->execute(array($_SESSION['id_user']));
+    $userData = $req_info->fetch();
 
- 
-// Requete tab
-$req_likes = $bdd->prepare('SELECT compteur_likes FROM etablissements');
-$req_likes->execute();
-$tab_cp_likes = $req_likes->fetchall();
 
-/*
-//Debuging
-var_dump($tab_cp_likes);
-*/ 
+    /*
+    // Requete tab 
+    $req_etablissements = $bdd->prepare('SELECT nom, descriptif FROM etablissements');
+    $req_etablissements->execute();
+    $tabfinals = $req_etablissements->fetchall();
 
+    
+    // Requete tab
+    $req_likes = $bdd->prepare('SELECT compteur_likes FROM etablissements');
+    $req_likes->execute();
+    $tab_cp_likes = $req_likes->fetchall();
+
+    /*
+    //Debuging
+    var_dump($tab_cp_likes);
+    */ 
+
+
+
+    //insert tableaux
+    $req_list = $bdd->query('SELECT nom, descriptif, dir_logo, compteur_likes, compteur_dislikes FROM etablissements');
+
+
+
+}
 ?>
  
 
@@ -49,13 +64,11 @@ var_dump($tab_cp_likes);
 <meta charset="utf-8"/>
 <link rel="icon" href="favicon.ico">
 <link href="css/dashboard.css" rel="stylesheet" type="text/css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-<link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+<style>
+@import url('https://fonts.googleapis.com/css?family=Dosis&display=swap');
+</style>
+</head>
 
-<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.no-icons.min.css" rel="stylesheet">
-<link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script>
 <script>
 $(document).ready(function(){
@@ -72,24 +85,17 @@ $mess_user_date = "";    //  Derniere date de connexion
 $mess_user_pending = ""; //  non lu
 ?>
 
-</head>
 <body>
-    <div class="container">
-        <section><!-- Section 1 - 2 rows  - header & Profile -->
-
-        <!-- include header -->
+<div id="main_container"><!-- Main container -->
         <?php include("header.php"); ?>
-        </section><!-- Fin section header --> 
+        <?php include("middle_section.php"); ?>
 
-        <!-- include midle section -->
-        <?php include("midle_section.php"); ?>
             <div class="row"><!-- Content 1000 px  1 col-->
-                <article class="col-lg-12">
+                <article>
+
                 </article>
             </div>
 
-            <!-- include footer -->   
         <?php include("footer.php"); ?>
-    </div><!--Fin de container -->
 </body>
 </html>
