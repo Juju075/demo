10c3c76f4b9340b9821bb136d3a7f8690da29a11
elgato <amzfba.1bestbuy@gmail.com>
2020-03-06 12:19:29 +0100

    update

--- header.php
+++ header.php
@@ -1,36 +1,51 @@
-<header class="row">
-                <div class="col-lg-8"><!-- Logo -->
-                    <a name="haut"></a>
-                    <img src="images/dashboard_01.jpg">
-                </div>
-                <div class="col-lg-4"><!-- Connexion membre -->
-                    <nav>
-                        <ul>
-                            <li><a href="profile.php">Mon profile</a>&nbsp;<a href="">Deconnexion</a><li>
-                        </ul>
-                    </nav>
-                   
-                </div><!-- Fin row -->
- </header>
- <div class="row">
-                <div class="col-lg-6"><!-- Profile picture -->
-                    <img src="images/profile_01.png" width="70px" height="70px"/>
-                    <p>Bonjour, <?php echo $_SESSION['prenom'] . ' ' .$_SESSION['nom_de_famille']; ?></p>
-                    <p>Derniere date de connexion:<? echo $mess_user; ?></p>
-                    <p>Message(s) non lu:<? echo $mess_user_pending; ?></p>   
-                </div>
-                
-            <section>
-                <div class="col-xs-2 col-sm-2 col-md-2 col-lg-1"><a href="?rank=comments"><img src="images/icon_01.png"></a></div>
-                <div class="col-xs-2 col-sm-2 col-md-2 col-lg-1"><a href="?rank=desc"><img src="images/icon_02.png"></a></div>
-                <div class="col-xs-2 col-sm-2 col-md-2 col-lg-1"><a href="?rank=insc"><img src="images/icon_03.png"></a></div>
-                <div class="col-xs-2 col-sm-2 col-md-2 col-lg-1"><a href="#"><img src="images/icon_04.png"></a></div>
-                <div class="col-xs-2 col-sm-2 col-md-2 col-lg-1"><a href="#"><img src="images/icon_05.png"></a></div>
-            </section>  
+<section><!-- Section header -->
 
-<div class="row">
-    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-    </div>
-</div>         
+        <div class="header"><!-- 1 row --> 
+
+            <div><!-- 1 item - logo st -->
+                <img src="images/dashboard_logo.jpg">
+            </div>
+
+            <div><!-- 2 nd item - Profile Deconnexion -->
+                <nav>
+                    <ul>
+                        <li><a href="profile.php">Mon profile</a>&nbsp;<a href="log_out.php">Deconnexion</a><li>
+                    </ul>
+                </nav>
+            </div> 
+
+        </div>
+        <div class="profile">    <!-- 1 row --> 
+
+                    <div class="">
+                        <img src="images/avatar/<?php echo $userData['avatar'];?>" class="avatar"/>
+                    </div>
+
+            <div>
+                <p>Bonjour, <?php echo ucfirst($_SESSION['prenom']) . ' ' . ucfirst($_SESSION['nom_de_famille']); ?></p>
+                <p>Dernière date de connexion:<? echo $mess_user; ?></p>
+                <p>Message(s) non lu:<? echo $mess_user_pending; ?></p>    
+            </div>
+
+            <div class="icones"><!-- Icones quieries-->
+                <a href="profile.php"><img src="images/icon_01.png" width="60px" height="60px"/></a>
+                <a href="notation.php?rank=ASC"><img src="images/icon_02.png" width="60px" height="60px"/></a>
+                <a href="notation.php?rank=DESC"><img src="images/icon_03.png" width="60px" height="60px"/></a>
                 
-            </div><!-- Fin row -->
+                <a href="#"><img src="images/icon_04.png" width="60px" height="60px"/></a>
+                <a href="#"><img src="images/icon_05.png"width="60px" height="60px"/></a>  
+            </div>
+            <div class="icones1">
+                <nav>
+                    <ul>
+                        <li><a href="#">Meilleurs commentaire.</a></li>
+                        <li><a href="#">Croissant</a></li>
+                        <li><a href="#">Decroissant</a></li>
+                        <li><a href="#">Fromage</a></li>
+                        <li><a href="#">Dossier</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div> 
+        <hr class="icones1" size="2" width="90%" color="red">
+    </section>  <!-- Fin section header--> 
