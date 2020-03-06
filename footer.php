5c200197a1ca055224117d36d36e35de91d232a2
elgato <amzfba.1bestbuy@gmail.com>
2020-03-06 11:04:27 +0100

    verif ok

--- footer.php
+++ footer.php
@@ -1,22 +1,9 @@
 <section><!-- Footer -->
-
-            <footer class="row">
-            <a href="#haut">Retour en haut de la page</a>
-            <img src="images/footer.jpg">
-                <div class="col-lg-4">
-                texte 1 ici
-                </div>
-                <div class="col-lg-4">
-                texte 1 ici
-                </div>
-                <div class="col-lg-4">
-                texte 1 ici
-                </div>
-
-                <div class="row">
-                <div class="col-lg-12">
-                <p>elgatodesign 2020 ©  Tous droits réservés.</p>
-                </div>
-                </div>
-            </footer>
-        </section>
+            <div><!-- 1 row --> 
+                <img src="images/footer.jpg" class="responsive"> 
+            </div>    
+    </section><!-- Fin section footer --> 
+</div>
+<div>
+    <p>elgatodesign 2020 © Tous droits réservés.</p>
+</div>
