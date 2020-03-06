99398064e7c3ca60b6ea9b9f2193f8e93c4a0a70
elgato <amzfba.1bestbuy@gmail.com>
2020-03-06 12:24:47 +0100

    update

--- php/execution.php
+++ php/execution.php
@@ -1,5 +1,4 @@
-<?php
-        
+<?php       
     if(isset($_POST['mot_de_passe'], $_POST['utilisateur']) AND !empty($_POST['mot_de_passe'] AND $_POST['utilisateur'])){ 
         
         //Verification login et pass identique 
@@ -18,57 +17,50 @@
         
         // vérifier la correspondance  Parametres: pass saisi vs pass stocke
         $Verif_pass= password_verify($_POST['mot_de_passe'], $resultat['mot_de_passe']);
+        $verif_activation;
        
-
+        
         if ($Verif_pass == FALSE) {
             header('location: http://extranet.gbaf.freeprofy.com/?connexion=false');
             
-        }elseif ($Verif_pass == TRUE) {
+        }    // A faire aussi verification cpt utilisateur activé
+        elseif ($Verif_pass == TRUE) {
             session_start();
-                /*$_SESSION['utilisateur'] = $_POST['utilisateur'];
-                $_SESSION['mot_de_passe'] = $resultat['mot_de_passe'];*/
+
+               //set cookie 
+               $login = $_POST['utilisateur'];
+               $pass = $resultat['mot_de_passe'];
+               setcookie('login', $login, time() * 365*24*3600, null,null,false,true);
+               setcookie('pass_hache', $pass, time() * 365*24*3600, null,null,false,true);
 
 
-                
-                $req_prenom = $bdd->prepare('SELECT prenom FROM banksters WHERE utilisateur = ? ');
-                $req_prenom->execute(array($_SESSION['utilisateur']));
-                $prenom = $req_prenom->fetch();
-                
-                
-                
-                $req_nom_de_famille = $bdd->prepare('SELECT nom FROM banksters WHERE utilisateur = ? ');
-                $req_nom_de_famille->execute(array($_SESSION['utilisateur']));
-                $nomfamille = $req_nom_de_famille->fetch();
-                
-                //Debuging
+
+                $req_name = $bdd->prepare('SELECT prenom, nom, id_user FROM banksters WHERE utilisateur = ? ');
+                $req_name->execute(array($_POST['utilisateur']));
+                $name = $req_name->fetchall();
+                var_dump($name);
+
+                //Debugging
                 var_dump($_SESSION['prenom']);
                 echo $prenom[0];
                 echo $nomfamille[0];
 
                 $_SESSION['utilisateur'] = $_POST['utilisateur'];
                 $_SESSION['mot_de_passe'] = $resultat['mot_de_passe'];
-                $_SESSION['prenom'] = $prenom[0];
-                $_SESSION['nom_de_famille'] = $nomfamille[0];
+                $_SESSION['prenom'] = $name[0]['prenom'];
+                $_SESSION['nom_de_famille'] = $name[0]['nom'];
+                $_SESSION['id_user'] = $name[0]['id_user'];
 
+                echo $_SESSION['id_user'];
+              
                 //Enregistre date de connexion (pour bdd derniere date de connexion.)
-
-
-
-
-
-
-
-
-
-
-
+                $req_date = $bdd->prepare('UPDATE banksters SET last_connexion = ? WHERE id_user = ?');
+                $req_date->execute(array(time(), $name[0]['id_user']));
 
                 header('location: http://extranet.gbaf.freeprofy.com/dashboard.php');
-                exit;
-        }
-        
+                exit();
+        }      
     }//Fin isset 
-        echo 'Fin de script ok php file'; 
-    
-        ?>
+        echo 'Fin de script';    
+?>
 
