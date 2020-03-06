099e6c4728ec3fdaee5e0a4fa438d02c47304da4
elgato <amzfba.1bestbuy@gmail.com>
2020-03-06 19:47:58 +0100

    delete

---
+++ mentions_legales.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html />
+<html>
+<head>
+<title>ma page</title>
+<meta charset="utf-8"/>
+<link rel="icon" href="favicon.ico">
+<link href="css/mentions.css" rel="stylesheet" type="text/css">
+<style>
+@import url('https://fonts.googleapis.com/css?family=Dosis&display=swap');
+</style>
+</head>
+
+<body>
+
+<p>Conformément aux dispositions de la loi n° 2004-575 du 21 juin 2004 pour la confiance en l’économie numérique, il est précisé aux utilisateurs du site legalplace.fr l’identité des différents intervenants dans le cadre de sa réalisation et de son suivi.
+<br>
+Edition du site
+Le site legalplace.fr est édité par la Groupement Banque Assurance Francais, société par actions simplifiée au capital de 10.051,50 euros, dont le siège social est situé 7 rue Marcel Dassault, 92100 Boulogne-Billancourt, immatriculée au registre du commerce et des sociétés sous le numéro d’identification unique 814 428 785 RCS Nanterre.
+<br>
+Responsable de publication
+Samuel Goldstein
+<br>
+Hébergeur
+Le site legalplace.fr est hébergé par la société Amazon Web Services LLC
+<br>
+Adresse: P.O. Box 81226, Seattle, WA 98108-1226
+<br>
+Le stockage des données personnelles des Utilisateurs est exclusivement réalisé sur les centre de données (« clusters ») localisés dans des Etats membres de l’Union Européenne de la société Amazon Web Services LLC
+<br>
+Nous contacter
+Par email : support@legalplace.fr
+<br>
+CNIL
+La Groupement Banque Assurance Francais conservera dans ses systèmes informatiques et dans des conditions raisonnables de sécurité une preuve de la transaction comprenant le bon de commande et la facture.
+<br>
+La Groupement Banque Assurance Francais a fait l’objet d’une déclaration à la CNIL sous le numéro 1986932.
+<br>
+Conformément aux dispositions de la
+</p>
+
+</body>
+</html>
+
+
+
---
+++ middle_section.php
@@ -0,0 +1,22 @@
+<section><!-- Middle section -->
+        <div class="middle_content"> <!-- 1 row --> 
+            <div>
+                <nav>
+                    <ul>
+                        <br>
+                        <li><a href="dashboard.php">Accueil</a></li>
+                        <li>Messagerie</li>
+                        <li><a href="newsfeed.php">Partenaires</a></li>
+                        <li><a href="https://fr.global-rates.com/taux-de-interets/libor/euro-europeen/euro-europeen.aspx" target="_blank"> Taux interbancaire Libord</a></li>
+                        <li><a href="notation.php">Notation</a></li>
+                        <br>
+                    </ul>
+                </nav>
+                <br>
+                <script type="text/javascript">window.onload = date_heure('date_heure');</script>
+            </div>
+            <div class="image_container"><img src="images/conseiller.jpg" class="responsive"></div>
+            <div class="centered">BIENVENUE SUR VOTRE ESPACE GBAF</div>
+        </div>
+
+    </section><!-- Fin middle section--> 
