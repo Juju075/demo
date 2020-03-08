978272137736cb83e70d13e263da08ac47817478
elgato <amzfba.1bestbuy@gmail.com>
2020-03-08 18:49:08 +0100

    update index ajout condition cpt valide 1

--- php/execution.php
+++ php/execution.php
@@ -1,42 +1,42 @@
 <?php       
-    if(isset($_POST['mot_de_passe'], $_POST['utilisateur']) AND !empty($_POST['mot_de_passe'] AND $_POST['utilisateur'])){ 
-        
-        //Verification login et pass identique 
-        $bdd = new PDO('mysql:host=localhost;dbname=mon_projet;charset=UTF8', 'dev06' ,'_cxeK9Dt)hkA', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-        $req = $bdd->prepare('SELECT mot_de_passe FROM banksters WHERE utilisateur = ?');
-        $req->execute(array($_POST['utilisateur']));
-        $resultat = $req->fetch();
 
 
-        //Debugging
-        var_dump($resultat);
-        echo $_POST['utilisateur'];
-        echo $_POST['mot_de_passe'];
-        echo $resultat['mot_de_passe'];
-        //-----------------------------
-        
-        // vérifier la correspondance  Parametres: pass saisi vs pass stocke
-        $Verif_pass= password_verify($_POST['mot_de_passe'], $resultat['mot_de_passe']);
-        $verif_activation;
-       
-        
-        if ($Verif_pass == FALSE) {
-            header('location: http://extranet.gbaf.freeprofy.com/?connexion=false');
-            
-        }    // A faire aussi verification cpt utilisateur activé
-        elseif ($Verif_pass == TRUE) {
-            session_start();
+if(isset($_POST['mot_de_passe'], $_POST['utilisateur']) AND !empty($_POST['mot_de_passe'] AND $_POST['utilisateur'])){ 
+    
+    //Verification login et pass identique 
+    $bdd = new PDO('mysql:host=localhost;dbname=mon_projet;charset=UTF8', 'dev06' ,'_cxeK9Dt)hkA', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+  
+    $req = $bdd->prepare('SELECT mot_de_passe, isemailconfirmed  FROM banksters WHERE utilisateur = ?');
+    $req->execute(array($_POST['utilisateur']));
+    $resultat = $req->fetch();
+    
+    echo '<pre>';
+    var_dump($resultat);
+    
+    $Verif_pass = password_verify(htmlspecialchars($_POST['mot_de_passe']), $resultat['mot_de_passe']);
+
+    echo $resultat['isemailconfirmed'];
+    echo '<pre>';
+
+    if ($Verif_pass == TRUE && $resultat['isemailconfirmed'] == 1) { // vérifier la correspondance  pass saisi et pass bdd.
+        echo '<pre>';
+
+        //enregister la date de connexion
+        $last_connexion = date("l");
+        $req_last_connexion = $bdd->prepare('UPDATE banksters SET last_connexion = ? WHERE utilisateur = ? ');
+        $req_last_connexion->execute(array($last_connexion, $_SESSION['utilisateur']));
+        echo 'jusquici tout vas bien 1';
+
+        session_start();
 
                //set cookie 
-               $login = $_POST['utilisateur'];
+               $login = htmlspecialchars($_POST['utilisateur']);
                $pass = $resultat['mot_de_passe'];
                setcookie('login', $login, time() * 365*24*3600, null,null,false,true);
                setcookie('pass_hache', $pass, time() * 365*24*3600, null,null,false,true);
 
-
-
                 $req_name = $bdd->prepare('SELECT prenom, nom, id_user FROM banksters WHERE utilisateur = ? ');
-                $req_name->execute(array($_POST['utilisateur']));
+                $req_name->execute(array($login));
                 $name = $req_name->fetchall();
                 var_dump($name);
 
@@ -45,22 +45,19 @@
                 echo $prenom[0];
                 echo $nomfamille[0];
 
-                $_SESSION['utilisateur'] = $_POST['utilisateur'];
-                $_SESSION['mot_de_passe'] = $resultat['mot_de_passe'];
+                $_SESSION['utilisateur'] = $login;
+                $_SESSION['mot_de_passe'] = $pass;
                 $_SESSION['prenom'] = $name[0]['prenom'];
                 $_SESSION['nom_de_famille'] = $name[0]['nom'];
                 $_SESSION['id_user'] = $name[0]['id_user'];
 
-                echo $_SESSION['id_user'];
-              
-                //Enregistre date de connexion (pour bdd derniere date de connexion.)
-                $req_date = $bdd->prepare('UPDATE banksters SET last_connexion = ? WHERE id_user = ?');
-                $req_date->execute(array(time(), $name[0]['id_user']));
-
                 header('location: http://extranet.gbaf.freeprofy.com/dashboard.php');
                 exit();
-        }      
-    }//Fin isset 
-        echo 'Fin de script';    
+      
+    }else{
+        $smg = 'Mauvais mot de pass ou login ou cpt non valide.';
+    }
+} //Fin isset 
+echo 'Fin de script'; 
 ?>
 
