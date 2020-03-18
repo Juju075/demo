95f08a5047f040b4f65c682754c2d3bb9f1684ef
elgato <amzfba.1bestbuy@gmail.com>
2020-03-18 09:19:11 +0100

    update

--- notation.php
+++ notation.php
@@ -1,64 +1,89 @@
 <?php
 session_start();
 
-$bdd = new PDO('mysql:host=localhost;dbname=mon_projet;charset=UTF8', 'dev06' ,'_cxeK9Dt)hkA', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-$req_info = $bdd->prepare('SELECT avatar FROM banksters WHERE id_user = ?');
-$req_info->execute(array($_SESSION['id_user']));
-$userData = $req_info->fetch();
-?>
+if(isset($_SESSION['id_user']) AND !empty ($_SESSION['id_user'])){
 
-<!DOCTYPE html/>
+    $bdd = new PDO('mysql:host=localhost;dbname=mon_projet;charset=UTF8', 'dev06' ,'_cxeK9Dt)hkA', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    $req_prenom = $bdd->prepare('SELECT prenom FROM banksters WHERE id_user = ? ');
+    $req_prenom->execute(array($_SESSION['id_user']));
+    $prenom = $req_prenom->fetch();
+
+    $req_nom_de_famille = $bdd->prepare('SELECT nom FROM banksters WHERE id_user = ? ');
+    $req_nom_de_famille->execute(array($_SESSION['id_user']));
+    $nomfamille = $req_nom_de_famille->fetch();
+
+
+    $likes = null;
+
+    $req_info = $bdd->prepare('SELECT avatar FROM banksters WHERE id_user = ?');
+    $req_info->execute(array($_SESSION['id_user']));
+    $userData = $req_info->fetch();
+    
+
+}else{
+    header('location: /index.php?c=non_connecte');
+}
+?>
+ 
+
+<!DOCTYPE html />
 <html>
 <head>
 <title>ma page</title>
 <meta charset="utf-8"/>
 <link rel="icon" href="favicon.ico">
-<link href="css/dashboard.css" rel="stylesheet" type="text/css">
-<script>
-<script>
-$(document).ready(function(){
-    $("#div3").fadeIn(900);
-});
-</script>
+<link href="css/dashboard_redesign_1.css" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Abel&display=swap" rel="stylesheet">
+<style>
+</style>
 </head>
 <body>
-    <div id="main_container"><!-- Main container -->
-            <section><!-- Section 1 - 2 rows  - header & Profile -->
-            <?php include("header.php"); ?>
-            <?php include("middle_section.php"); ?>
 
-                <div class="row"><!-- Content 1000 px  1 col-->
-                    <article>
+<div id="main_container"><!-- Main container -->
 
-                        <div class="bloc_notation">
-                            <?php
-                                $bdd = new PDO('mysql:host=localhost;dbname=mon_projet;charset=UTF8', 'dev06' ,'_cxeK9Dt)hkA', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-                                $req_list = $bdd->query('SELECT nom, descriptif, dir_ph_headquarter, compteur_likes, compteur_dislikes FROM etablissements ORDER BY compteur_likes ASC');
-                                while($tab_fiches = $req_list->fetch()){ 
-                            ?>    
-                                <table>
-                                    <tr>
-                                        <th><img src="<?php echo 'images/'. $tab_fiches['dir_ph_headquarter'];?>" width="197" height="100"/></th>
-                                        <th><h1><?php echo $tab_fiches['nom']; ?></h1></th>
-                                    </tr>
-                                    <tr>
-                                        <td colspan="2" >                           
-                                        <p><?php echo $tab_fiches['descriptif']; ?></p></td>
-                                    </tr> 
-                                    <tr>
-                                        <td><button type="button" onclick="toggle_div(this,'test-1');" title="Afficher la suite">+</button>
-                                        <a href="template_details.php?etablissement=<?= $tab_fiches['nom'];?>">Lire la suite</a></td>
-                                    </tr>   
-                                </table>
-                            <?php
-                                } // Fin de boucle
-                            ?>           
-                        </div>    
+    <?php include("header_redesign.php"); ?> <!-- ouverture et fermeture section -->
 
-                    </article>
-                </div>
+    <div class="row_container space-around">
+        <div class="item item-1"><a href="dashboard.php">Accueil</a></div>
+        <div class="item item-2"><a href="/profile.php">Profil</a></div>
+        <div class="item item-3"><a href="notation.php">Partennaires</a></div>
+        <div class="item item-4"><a href="add_etablissement.php">Ajout établissement</a></div>
+    </div>
 
-            <?php include("footer.php"); ?>
-          
+ <div class="row"><!-- Content 1000 px  1 col-->
+    <article>
+
+        <?php
+            $bdd = new PDO('mysql:host=localhost;dbname=mon_projet;charset=UTF8', 'dev06' ,'_cxeK9Dt)hkA', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+            $req_list = $bdd->query('SELECT nom, descriptif, dir_ph_headquarter FROM etablissements');
+                                                              
+            while($tab_fiches = $req_list->fetch()){ 
+            ?>  
+
+            <div class="row_container space-around">
+                <div>
+                    <div class="item item1"><img src="<?php echo 'images/'. $tab_fiches['dir_ph_headquarter'];?>" width="197px" height="100px" alt="logo_etablissement"/></div>
+                    <div class="item item2"><h1><?php echo $tab_fiches['nom']; ?></h1></div>
+                <div>
+                <div>    
+                    <div class="item item3"><p><?php echo $tab_fiches['descriptif']; ?></p></div>
+                <div>    
+                <div>
+                    <div class="item item4"><a href="template_details.php?etablissement=<?= $tab_fiches['nom'];?>">Lire la suite</a></div>
+                <div>
+            </div>
+        <?php
+        } // Fin de boucle
+        ?> 
+         
+
+    </article>
+</div>
+
+    <?php include("footer.php"); ?> <!-- ouverture et fermeture section -->
+    
+   
+</div> 
+<!--  -->
 </body>
 </html>
