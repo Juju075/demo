8f54c37ab36b1051d69421cef4e4340808ac61de
elgato <amzfba.1bestbuy@gmail.com>
2020-03-18 09:11:48 +0100

    update

--- dashboard.php
+++ dashboard.php
@@ -1,59 +1,31 @@
-
- <?php
+<?php
 session_start();
-if(isset($_SESSION['utilisateur']) AND !empty ($_SESSION['utilisateur'])){
 
+if(isset($_SESSION['id_user']) AND !empty ($_SESSION['id_user'])){
 
-    //interdir l(acces a la page si session null)
     $bdd = new PDO('mysql:host=localhost;dbname=mon_projet;charset=UTF8', 'dev06' ,'_cxeK9Dt)hkA', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-    $req_prenom = $bdd->prepare('SELECT prenom FROM banksters WHERE utilisateur = ? ');
-    $req_prenom->execute(array($_SESSION['utilisateur']));
+    $req_prenom = $bdd->prepare('SELECT prenom FROM banksters WHERE id_user = ? ');
+    $req_prenom->execute(array($_SESSION['id_user']));
     $prenom = $req_prenom->fetch();
 
-    $req_nom_de_famille = $bdd->prepare('SELECT nom FROM banksters WHERE utilisateur = ? ');
-    $req_nom_de_famille->execute(array($_SESSION['utilisateur']));
+    $req_nom_de_famille = $bdd->prepare('SELECT nom FROM banksters WHERE id_user = ? ');
+    $req_nom_de_famille->execute(array($_SESSION['id_user']));
     $nomfamille = $req_nom_de_famille->fetch();
 
-    /*
-    //Debuging
-    var_dump($_SESSION['prenom']);
-    echo $prenom[0];
-    echo $nomfamille[0];
-    //------------------------------
-    */ 
 
     $likes = null;
 
     $req_info = $bdd->prepare('SELECT avatar FROM banksters WHERE id_user = ?');
     $req_info->execute(array($_SESSION['id_user']));
     $userData = $req_info->fetch();
-
-
-    /*
-    // Requete tab 
-    $req_etablissements = $bdd->prepare('SELECT nom, descriptif FROM etablissements');
-    $req_etablissements->execute();
-    $tabfinals = $req_etablissements->fetchall();
-
     
-    // Requete tab
-    $req_likes = $bdd->prepare('SELECT compteur_likes FROM etablissements');
-    $req_likes->execute();
-    $tab_cp_likes = $req_likes->fetchall();
-
-    /*
-    //Debuging
-    var_dump($tab_cp_likes);
-    */ 
-
-
-
     //insert tableaux
     $req_list = $bdd->query('SELECT nom, descriptif, dir_logo, compteur_likes, compteur_dislikes FROM etablissements');
 
-
-
+}else{
+    header('location: /index.php?c=non_connecte');
 }
+
 ?>
  
 
@@ -63,39 +35,34 @@ if(isset($_SESSION['utilisateur']) AND !empty ($_SESSION['utilisateur'])){
 <title>ma page</title>
 <meta charset="utf-8"/>
 <link rel="icon" href="favicon.ico">
-<link href="css/dashboard.css" rel="stylesheet" type="text/css">
+<link href="css/dashboard_redesign_1.css" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Abel&display=swap" rel="stylesheet">
 <style>
-@import url('https://fonts.googleapis.com/css?family=Dosis&display=swap');
 </style>
 </head>
-
-<script>
-<script>
-$(document).ready(function(){
-    $("#div3").fadeIn(900);
-});
-</script>
-
-
-<? 
-$user = "cookie";
-$img =  "images/profile_ . $user . \.png"; //  Avatar
-$img_alt = 'photo de profile';
-$mess_user_date = "";    //  Derniere date de connexion
-$mess_user_pending = ""; //  non lu
-?>
-
 <body>
+
 <div id="main_container"><!-- Main container -->
-        <?php include("header.php"); ?>
-        <?php include("middle_section.php"); ?>
 
-            <div class="row"><!-- Content 1000 px  1 col-->
-                <article>
+    <?php include("header_redesign.php"); ?> <!-- ouverture et fermeture section -->
 
-                </article>
-            </div>
+    <div class="row_container space-around">
+      <div class="item item-1"><a href="dashboard.php">Accueil</a></div>
+      <div class="item item-2"><a href="/profile.php">Profil</a></div>
+      <div class="item item-3"><a href="notation.php">Partennaires</a></div>
+      <div class="item item-4"><a href="add_etablissement.php">Ajout établissement</a></div>
+    </div>
 
-        <?php include("footer.php"); ?>
+ <div class="row"><!-- Content 1000 px  1 col-->
+    <article class="justify">
+        <P>La GBAF promeut l’activité bancaire et financière sur les marchés français, européens et internationaux, et définit les positions et propositions de la profession vis-à-vis des pouvoirs publics et des autorités du domaine économique et financier. Elle diffuse également des normes, bonnes pratiques et recommandations professionnelles et met son expérience à la disposition de ses membres. La GBAF  a aussi pour mission d’informer les banques adhérentes de toute question relative à leurs activités.</p>
+    </article>
+</div>
+
+    <?php include("footer.php"); ?> <!-- ouverture et fermeture section -->
+    
+   
+</div> 
+<!--  -->
 </body>
 </html>
