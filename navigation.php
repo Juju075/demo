d67fb435db9f040a2af61ce481f569231f36ff6e
elgato <amzfba.1bestbuy@gmail.com>
2020-03-27 15:31:43 +0100

    creation page navigation

--- navigation.php
+++ navigation.php
@@ -1,1 +1,6 @@
-
+<nav class="row_container space-around">
+    <div class="item item-1"><a href="dashboard.php">Accueil</a></div>
+    <div class="item item-2"><a href="/profile.php">Profil</a></div>
+    <div class="item item-3"><a href="notation.php">Partennaires</a></div>
+    <div class="item item-4"><a href="add_etablissement.php">Ajout établissement</a></div>
+</nav>
