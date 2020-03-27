2181bf1cd94a97cdcf421f00360aff8898b3b2ab
elgato <amzfba.1bestbuy@gmail.com>
2020-03-27 11:27:24 +0100

    modification classes name

--- index.php
+++ index.php
@@ -1,11 +1,7 @@
 <?php
-         //remplissage automatique login et mot de passe  
-           
-           if(isset($_COOKIE['user_id'])){
-
-                //verifie dans la bdd si 
-                echo 'Votre ID de session est le ' .$_COOKIE['user_id'];
-            }
+if(isset($_COOKIE['user_id'])){
+    echo 'Votre ID de session est le ' .$_COOKIE['user_id'];
+}
 ?>
 
 <!DOCTYPE html>
@@ -26,26 +22,26 @@
                 <img src="images/cadre.jpg" alt="image_de_couverture" height="500">
             </div><!-- Fin elements 1 -->
             <div class="bloc_elements"><!-- elements 2-->
-                <div class="sous_elements"><img src="images/gbaf_logo.jpg" alt="logo_federation_gbaf" height="130"></div><!-- Fin sous_elements-1 -->
-                <div class="sous_elements"><p>BIENVENUE<br>
+                <div class="item"><img src="images/gbaf_logo.jpg" alt="logo_federation_gbaf" height="130"></div><!-- Fin item-1 -->
+                <div class="item"><p>BIENVENUE<br>
                         sur la page d'identification de votre Extranet<br> (espace réservé aux adhérents GBAF).<br>
                         En vous connectant sur ce site,vous acceptez <br> l'utilisation decookies à des fins de mesure d'audience.<br> <strong><a href="https://www.cnil.fr/professionnel" target="_blank">En savoir plus</a></strong></p>
-                </div><!-- Fin sous_elements-2 -->
-                <div class="sous_elements">
+                </div><!-- Fin item-2 -->
+                <div class="item">
                     <form method="post" action="php/execution.php">
                             <input  type="text" name="utilisateur" placeholder="Login" required>
                             <input type= "password" name= "mot_de_passe" placeholder="Votre mot de passe" required/><br>
                             <br><input class="envoyer" type= "submit" value=" Valider"/>
                     </form>
-                </div><!-- Fin sous_elements-3 -->
-                <div class="sous_elements">
+                </div><!-- Fin item-3 -->
+                <div class="item">
                     <nav>
                         <ul>
                             <li><a href="inscription.php" >S'inscrire.</a></li>
                             <li><a href="lost.php" >Mot de passe oublié?.</a></li>
                         </ul>
                     </nav>
-                </div><!-- Fin sous_elements-4 -->
+                </div><!-- Fin item-4 -->
                 <p class="erreur"><?php if ($_GET['passe'] == 'non_valide') {
                     echo 'Vous avez rentré une mauvaise<br> combinaison login et mot de passe<br> ou votre cpt n\'est pas activé.';
                 }elseif ($_GET['c'] == 'non_connecte') {
@@ -55,10 +51,9 @@
                 } ?></p>
             </div><!-- Fin elements 2-->
         </div><!-- Fin main wrap -->
-
         <div id="footer_container">
-            <footer class="sous_elements">
-                <h5>Le Groupement Banque Assurance Français - CONFIDENTIEL SAUF MENTION CONTRAIRE. -</h5> <a href="mentions_legales.php">Mentions légales</a>
+            <footer class="item">
+                <h5>Le Groupement Banque Assurance Français - CONFIDENTIEL SAUF MENTION CONTRAIRE. -</h5><a href="mentions_legales.php">Mentions légales</a>
             </footer>   
         </div><!-- Fin container 2-->                       
     </body>
