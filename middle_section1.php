835fe7025f14cd754f50afa11b96bb2ad3a3d18e
Juju075 <59140742+Juju075@users.noreply.github.com>
2020-03-30 16:48:34 +0200

    changement de nom

--- middle_section1.php
+++
@@ -1,8 +0,0 @@
-    <nav class="row_container space-around">
-        <ul>
-            <li class="item item-1"><a href="dashboard.php" title="Page Accueil">Accueil</a></li>
-            <li class="item item-2"><a href="/profile.php" title="Page profil">Profil</a></li>
-            <li class="item item-3"><a href="notation.php" title="Page etablissements">Partennaires</a></li>
-            <li class="item item-4"><a href="add_etablissement.php" title="Page ajout etablissement">Ajouter un etablissement</a></li>                       
-        </ul>
-    </nav>
